26039e3baf9a5dc624d79a37700d3d9e6abf405f
franbarreneche <franbarreneche@gmail.com>
2024-10-06 18:00:28 -0300

    feat: adds toast notification service

---
+++ src/app/shared/toast-notification.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastNotificationService {
+
+  constructor(
+    private readonly toast: MatSnackBar,
+  ) { }
+
+  showError(message: string): void {
+    this.toast.open(message, 'Close', {duration: 4000});
+  }
+
+  showSuccess(message: string): void {
+    this.toast.open(message, 'Close', {duration: 4000});
+  }
+}
